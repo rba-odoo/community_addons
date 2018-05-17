30cfc86074f70cac814a136625a84e589d4c98ab
Ronak Baxi <rbaodoo@gmail.com>
2018-05-17 14:36:17 +0530

    MIG: 11.0

--- track_all_fields/__manifest__.py
+++ track_all_fields/__manifest__.py
@@ -11,7 +11,7 @@
  1. Go to models views (Settings > Data Structure > Models)
  2. click on model and enable "Track all fields".
 
-Tested on Odoo 10.0 d8083b42704b3a5eb7f21ccd743a8265787f6caa
+Tested on Odoo 11.0 77aa313b141b9b4c79fa8e24ed0984a9d5c955fe
     """,
     'author': "Ronak Baxi",
     'depends': ['mail'],
